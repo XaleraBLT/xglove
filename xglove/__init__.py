7a11f2e579c2bde61307460d135ab27e25b80f93
Xalera <valera-ch.19.20@mail.ru>
2025-08-31 13:57:23 +0300

    Beta2

--- xglove/__init__.py
+++ xglove/__init__.py
@@ -1,3 +1,13 @@
 from . import connectors
 from . import utils
-from .glove import *
+
+try:
+    from .glove import *
+except ImportError:
+    class Glove:
+        def __init__(self, *args, **kwargs):
+            raise AttributeError("Аппаратная часть XGlove не поддерживается на данном устройстве. Используется "
+                                 "заглушка.")
+
+        def __str__(self):
+            return "Аппаратная часть XGlove не поддерживается на данном устройстве. Используется заглушка."
